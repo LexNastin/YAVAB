3b308ff39d599c3f6c053f50ae6d86a33da21199
ArchGryphon9362 <32967396+ArchGryphon9362@users.noreply.github.com>
2020-12-20 23:25:00 +0000

    Bug Fixes (Sorta): Custom Embed Change Options (+ Slight Edit Changes)

--- interactions/admin.js
+++ interactions/admin.js
@@ -37,33 +37,28 @@ module.exports = class admin {
 								type: 4
 							},
 							{
-								name: 'footer_text',
-								description: 'Footer Text',
+								name: 'footer',
+								description: 'Footer Object',
 								type: 3
 							},
 							{
-								name: 'footer_icon_url',
-								description: 'Footer Icon URL',
+								name: 'image',
+								description: 'Image Object',
 								type: 3
 							},
 							{
-								name: 'author_name',
-								description: 'Author Name',
+								name: 'video',
+								description: 'Video Object',
 								type: 3
 							},
 							{
-								name: 'author_url',
-								description: 'Author Name',
-								type: 3
-							},
-							{
-								name: 'author_icon_url',
-								description: 'Author Name',
+								name: 'author',
+								description: 'Author Object',
 								type: 3
 							},
 							{
 								name: 'fields',
-								description: 'Array of Fields',
+								description: 'Array of Field Objects',
 								type: 3
 							}
 						]
@@ -71,12 +66,60 @@ module.exports = class admin {
 					{
 						name: 'edit',
 						description: 'Edit an Embed',
-						type: 1
-					},
-					{
-						name: 'delete',
-						description: 'Delete an Embed',
-						type: 1
+						type: 1,
+						options: [
+							{
+								name: 'message_id',
+								description: 'Embed',
+								type: 4,
+								required: true
+							},
+							{
+								name: 'title',
+								description: 'Title of Embed',
+								type: 3
+							},
+							{
+								name: 'description',
+								description: 'Description of Embed',
+								type: 3
+							},
+							{
+								name: 'url',
+								description: 'URL of Embed',
+								type: 3
+							},
+							{
+								name: 'color',
+								description: 'Color of Embed',
+								type: 4
+							},
+							{
+								name: 'footer',
+								description: 'Footer Object',
+								type: 3
+							},
+							{
+								name: 'image',
+								description: 'Image Object',
+								type: 3
+							},
+							{
+								name: 'video',
+								description: 'Video Object',
+								type: 3
+							},
+							{
+								name: 'author',
+								description: 'Author Object',
+								type: 3
+							},
+							{
+								name: 'fields',
+								description: 'Array of Field Objects',
+								type: 3
+							}
+						]
 					}
 				]
 			}
@@ -85,65 +128,93 @@ module.exports = class admin {
 
 	async run(client, interaction, options) {
 		// JSON.parse("[" + string + "]")
+
+		function sleep(ms) {
+			return new Promise(resolve => setTimeout(resolve, ms));
+		 }
 		
 		if(!(interaction.member.permissions & 8)) return;
+		let args = {};
 
 		switch (options[0].name) {
 			case 'cembed':
 				switch (options[0].options[0].name) {
 					case 'create':
-						let args = {};
-
 						if (options[0].options[0].options) {
 							options[0].options[0].options.forEach(option => {
 								args[option.name] = option.value
 							});
 						}
 
+						if (args['footer']) {
+							args['footer'] = JSON.parse(args['footer']);
+						}
+						if (args['image']) {
+							args['image'] = JSON.parse(args['image']);
+						}
+						if (args['video']) {
+							args['video'] = JSON.parse(args['video']);
+						}
+						if (args['author']) {
+							args['author'] = JSON.parse(args['author']);
+						}
 						if (args['fields']) {
-							args['fields'] = JSON.parse(args['fields'])
+							args['fields'] = JSON.parse(args['fields']);
 						}
 
-						client.api.interactions(interaction.id, interaction.token).callback.post({data: {
+						await client.api.interactions(interaction.id, interaction.token).callback.post({data: {
 							type: 4,
 							data: {
-								embeds: [
-									{
-										title: args['title'],
-										description: args['description'],
-										url: args['url'],
-										color: args['color'],
-										footer: {
-											text: args['footer_text'],
-											icon_url: args['footer_icon_url']
-										},
-										author: {
-											name: args['author_name'],
-											url: args['author_url'],
-											icon_url: args['author_icon_url']
-										},
-										fields: args['fields']
-									}
-								]
+								content: 'Done'
 							}
-						}});
+						}}).catch((err) => {console.log(err)});
+
+						await client.channels.fetch(interaction.channel_id)
+						.then(channel => {
+							channel.send('', {
+								embed: {
+									rich: true,
+									title: args['title'],
+									description: args['description'],
+									url: args['url'],
+									color: args['color'],
+									footer: args['footer'],
+									image: args['image'],
+									video: args['video'],
+									author: args['author'],
+									fields: args['fields']
+								}
+							})
+						}).catch((err) => {console.log(err)});
+
+						await sleep(2000);
+
+						client.api.webhooks(client.user.id, interaction.token).messages('@original').delete().catch((err) => {console.log(err)});
+
 						break;
 
 					case 'edit':
+						if (options[0].options[0].options) {
+							options[0].options[0].options.forEach(option => {
+								args[option.name] = option.value
+							});
+						}
+
+						client.channels.fetch(interaction.channel_id)
+						.then(channel => {
+							channel.messages.fetch(args['message_id'])
+							.then(message => {
+								console.log(message);
+							}).catch((err) => {console.log(err)});
+						}).catch((err) => {console.log(err)});
+
 						client.api.interactions(interaction.id, interaction.token).callback.post({data: {
 							type: 4,
 							data: {
 								content: 'editedn\'t'
 							}
 						}});
-
-					case 'delete':
-						client.api.interactions(interaction.id, interaction.token).callback.post({data: {
-							type: 4,
-							data: {
-								content: 'delete yourself :)'
-							}
-						}});
+						break;
 				
 					default:
 						break;
