79d140ca003830bc3adcacc845a0fac6f881b803
ArchGryphon9362 <32967396+ArchGryphon9362@users.noreply.github.com>
2021-02-21 00:43:13 +0000

    Create messageDeleteBulk.js
    Allow bulk message deletion to be logged.

---
+++ events/messageDeleteBulk.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+
+module.exports = {
+	name: 'messageDelete',
+	run: async (client, messages) => {
+		let attachments = [];
+
+		currentDelLog = JSON.parse(fs.readFileSync('delLog.json'));
+
+		await messages.forEach(message => {
+			message.attachments.forEach(attachment => {
+				attachments.push(attachment.url);
+			});
+			message.attachmentUrls = attachments;
+
+			currentDelLog[message.createdTimestamp.toString()] = message
+		});
+		fs.writeFileSync('delLog.json', JSON.stringify(currentDelLog));
+		//fs.writeFileSync(`delLog/${message.author.username}_${message.author.id}_${message.createdTimestamp}.json`, JSON.stringify(message));
+	}
+}
